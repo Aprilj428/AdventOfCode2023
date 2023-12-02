cfc0240015654a31e6f7a23c2302ed3387d8b485
April <april.jackson428@gmail.com>
2023-12-02 10:46:15 -0600

    feat: day 1 solution

--- AdventOfCode2023Day1/Program.cs
+++ AdventOfCode2023Day1/Program.cs
@@ -1,3 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using var streamReader = new StreamReader("./input.txt");
+int total = 0;
+while (!streamReader.EndOfStream)
+{
+    var currentLine = streamReader.ReadLine();
+    if (string.IsNullOrWhiteSpace(currentLine))
+        throw new Exception("Unexpected line encountered");
 
-Console.WriteLine("Hello, World!");
+    char? lastParsedCharacter = null;
+    var matchedCharacters = new List<char>();
+    foreach (var character in currentLine.ToCharArray())
+    {
+        if (!IsInteger(character))
+            continue;
+
+        lastParsedCharacter = character;
+
+        if (IsFirstInteger(matchedCharacters))
+        {
+            matchedCharacters.Add(character);
+        }
+    }
+
+    if (lastParsedCharacter is null)
+        throw new Exception("No matching characters found for line");
+    
+    matchedCharacters.Add(lastParsedCharacter.Value);
+
+    var lineValue = int.Parse(string.Join("", matchedCharacters));
+    total += lineValue;
+}
+
+Console.WriteLine($"Total found: {total}");
+
+bool IsInteger(char character) => int.TryParse(character.ToString(), out _);
+bool IsFirstInteger(ICollection<char> matchedCharacters) => matchedCharacters.Count == 0;
